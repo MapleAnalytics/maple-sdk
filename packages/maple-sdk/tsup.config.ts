5bbd8090ae17ffaed012e647a47f8d9a3658daa9
Makisuo <31933546+Makisuo@users.noreply.github.com>
2023-05-13 14:01:49 +0200

    xd

--- packages/maple-sdk/tsup.config.ts
+++ packages/maple-sdk/tsup.config.ts
@@ -2,11 +2,16 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig((opts) => ({
 	entryPoints: ['src/index.ts', 'src/react.tsx', 'src/tracker.ts'],
-	format: ['cjs'],
+	format: ['cjs', 'esm'],
 	clean: !opts.watch,
 	outDir: 'dist',
-	sourcemap: true,
 	target: 'es2017',
+	treeshake: true,
+	sourcemap: 'inline',
+	minify: true,
+	dts: true,
+	splitting: false,
+	external: ['react'],
 	esbuildOptions(options) {
 		options.banner = {
 			js: '"use client"',
