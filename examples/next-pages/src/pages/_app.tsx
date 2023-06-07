4c458f546c96853828d8ac15822fb77b8b2eb0d9
Makisuo <31933546+Makisuo@users.noreply.github.com>
2023-06-07 11:03:23 +0200

    xd

--- examples/next-pages/src/pages/_app.tsx
+++ examples/next-pages/src/pages/_app.tsx
@@ -1,10 +1,7 @@
 import "@/styles/globals.css"
 import type { AppProps } from "next/app"
 
-import xd from "maple-sdk"
-
 export default function App({ Component, pageProps }: AppProps) {
-	console.log(xd)
 	return (
 		<>
 			<Component {...pageProps} />
